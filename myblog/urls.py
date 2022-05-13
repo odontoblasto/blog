b60a75c03b1439b8f1c29c69042deb2b774c0ba5
odontoblasto <cedfv@cin.ufpe.br>
2022-05-13 09:53:39 -0300

    base_html uses

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,11 @@
-from . import views
+#from . import views
 from django.urls import path
+from .views import HomeView,ArticleDetailView
 
 urlpatterns = [
   
-    path('',views.home,name='home'),
+    #path('',views.home,name='home'),
+    path('', HomeView.as_view(), name='home'),
+    path('article/<int:pk>',
+    ArticleDetailView.as_view(),name='article-detail'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Post
+
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html',{})
+#def home(request):
+#   return render(request, 'home.html',{})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'author_details.html'
+
+
+
+
