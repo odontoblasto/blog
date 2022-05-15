572a71426e23aaa1bde6416688b7d79f409d0fb8
odontoblasto <cedfv@cin.ufpe.br>
2022-05-15 18:32:26 -0300

    bootstrap and styling

---
+++ myblog/forms.py
@@ -0,0 +1,17 @@
+from logging import PlaceHolder
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','title_tag','author','body')
+
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control','placeholder':'This is a placeholder on Title'}),
+            'title_tag':forms.TextInput(attrs={'class':'form-control'}), 
+            'author':forms.Select(attrs={'class':'form-control'}), 
+            'body':forms.Textarea(attrs={'class':'form-control'}), 
+          
+        }#bootstrapify....styling....others attrs in the python dict...
--- myblog/views.py
+++ myblog/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
 
 #def home(request):
-#   return render(request, 'home.html',{})
+#   return render(request, 'home.html',{}) #create from def
 
-class HomeView(ListView):
+class HomeView(ListView):#create from a class
     model = Post
     template_name = 'home.html'
 
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'author_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    #fields ='__all__'
+    #fields = ('title','title_tag','body')#python list of attributes
+
 
 
 
